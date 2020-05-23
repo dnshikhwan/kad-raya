c713166de3f95e35c7e3e364e18b65ce4e0e20d5
danish ikhwan <danishikhwanneymar@gmail.com>
2020-05-24 00:51:16 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app" class="min-h-screen">
-    <div class="flex flex-col md:flex-row md:justify-center">
-    <header class="flex justify-center">
-      <h1 class="text-5xl font-bold mt-10 tracking-wide text-center">Happy Eid <br> Mubarak</h1>
+    <div class="flex flex-col md:flex-row md:justify-between">
+    <header class="flex justify-center md:flex-col md:pl-40">
+      <h1 class="text-5xl font-bold mt-10 tracking-wide text-center md:pl-16">Happy Eid <br> Mubarak</h1>
+      <button class="bg-green-300 py-2 shadow-lg border border-gray-800 rounded-lg md:flex-shrink hidden">Duit Raya</button>
     </header>
     <section class="flex justify-center">
-      <img src="./assets/raya.jpg" alt="raya.jpg">
+      <img class="md:pr-10" src="./assets/raya.jpg" alt="raya.jpg">
     </section>
-    <div class="flex justify-center">
-      <button class="bg-green-300 rounded-lg py-2 px-4 shadow-md border border-gray-600 md:hidden">
-        <a href="#">Duit Raya</a>
-      </button>
+    <div class="flex justify-center md:hidden">
+
     </div>
     </div>
-    <footer class="flex justify-end md:hidden">
-      <h1 class="px-3 font-semibold text-gray-800">@confirmnish</h1>
+    <footer class="w-full text-center pt-10 pin-b">
+      <h1 class="px-3 font-bold text-gray-800">@Danish Ikhwan</h1>
     </footer>
   </div>
 </template>
