26e21b61ea4fb6f26ed02f356535b5291f2c3190
danish ikhwan <danishikhwanneymar@gmail.com>
2020-05-23 22:18:29 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="min-h-screen">
+    <div class="flex flex-col md:flex-row md:justify-center">
+    <header class="flex justify-center">
+      <h1 class="text-5xl font-bold mt-10 tracking-wide text-center">Happy Eid <br> Mubarak</h1>
+    </header>
+    <section class="flex justify-center">
+      <img src="./assets/raya.jpg" alt="raya.jpg">
+    </section>
+    <div class="flex justify-center">
+      <button class="bg-green-300 rounded-lg py-2 px-4 shadow-md border border-gray-600 md:hidden">
+        <a href="#">Duit Raya</a>
+      </button>
+    </div>
+    </div>
+    <footer class="flex justify-end md:hidden">
+      <h1 class="px-3 font-semibold text-gray-800">@confirmnish</h1>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
+<style src="./css/tailwind.css">
+</style>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    background-color: #E1EFBE;
+  }
 </style>
